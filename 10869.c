b3367390e5c1ab36889e4c24ab5c3b57094e5986
woojoo1 <116423334+woojoo1@users.noreply.github.com>
2022-11-05 16:59:17 +0900

    Create 10869.c

---
+++ 10869.c
@@ -0,0 +1,17 @@
+int main()
+{
+    int A;
+    int B;
+    scanf("%d %d\n", &A, &B);
+    int c = A+B;
+    printf("%d\n", c);
+    c = A-B;
+    printf("%d\n", c);
+    c = A*B;
+    printf("%d\n", c);
+    c = A/B;
+    printf("%d\n", c);
+    c = A%B;
+    printf("%d\n", c);
+    return 0;
+}
