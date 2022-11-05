528193eda098671ce1445f060b1517394bd63536
woojoo1 <116423334+woojoo1@users.noreply.github.com>
2022-11-05 16:58:48 +0900

    Create 9498.c

---
+++ 9498.c
@@ -0,0 +1,26 @@
+int main()
+{
+    int a;
+    scanf("%d", &a);
+    if(100 >= a && a >= 90)
+    {
+         printf("A");
+    }
+    else if(90 > a && a >= 80)
+    {
+        printf("B");
+    }
+    else if(80 > a && a >= 70)
+    {
+        printf("C");
+    }
+    else if(70 > a && a >= 60)
+    {
+        printf("D");
+    }
+    else
+    {
+        printf("F");
+    }
+    return 0;
+}
